771da29d2b84a5a189467f636b871a1a5b366e9c
ezequielmenor <ezequielmenor@gmail.com>
2025-10-08 19:30:46 +0200

    Cambios Bol2/Ej3 Controller

---
+++ Boletin02/src/com/ezequielmenor/tema01/act3/Controller.java
@@ -0,0 +1,56 @@
+package com.ezequielmenor.tema01.act3;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
+
+public class Controller {
+    private static final String RUTA = "/Users/ezequielmenor/2DAM/AccesoDatos/tema01/ficheros/usuario.properties";
+    private static final String CLAVE = "password";
+    private final Properties prop = new Properties();
+    private final File f = new File(RUTA);
+
+    public Controller() throws Exception{
+        if(!f.exists()){
+            f.getParentFile().mkdirs();
+            prop.setProperty(CLAVE, hashSHA1("S3cret@"));
+            prop.store(new FileOutputStream(f), "Archivo de usuario");
+        }else {
+            prop.load(new FileInputStream(f));
+        }
+    }
+
+    public boolean validarAcceso (String pass) throws NoSuchAlgorithmException {
+        return prop.getProperty(CLAVE).equals(hashSHA1(pass));
+    }
+
+    public boolean cambiarContrasena(String actual, String nueva) throws Exception {
+        if(!prop.getProperty(CLAVE).equals(hashSHA1(actual))) return false;
+        if(!validarContrasena(nueva)) return false;
+
+        prop.setProperty(CLAVE, hashSHA1(nueva));
+        prop.store(new FileOutputStream(f), "Archivo de usuario");
+        return true;
+    }
+
+    public static String hashSHA1(String input) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        byte[] hashBytes = md.digest(input.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    public static boolean validarContrasena(String pass) {
+        return pass.length() >= 8 &&
+                pass.matches(".*[A-Z].*") &&
+                pass.matches(".*[a-z].*") &&
+                pass.matches(".*[0-9].*") &&
+                pass.matches(".*[^a-zA-Z0-9].*");
+    }
+}
---
+++ Boletin02/src/com/ezequielmenor/tema01/act3/Main.java
@@ -0,0 +1,66 @@
+package com.ezequielmenor.tema01.act3;
+
+
+import java.util.Scanner;
+import com.ezequielmenor.tema01.act3.Controller;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try {
+            Scanner sc = new Scanner(System.in);
+            Controller controlador = new Controller();
+
+            System.out.println("**********************");
+            System.out.println("1. Validar Acceso");
+            System.out.println("**********************");
+            System.out.print("Elige opción: ");
+            int opcion = Integer.parseInt(sc.nextLine());
+
+            if (opcion == 1) {
+                System.out.print("Introduce la contraseña: ");
+                String pass = sc.nextLine();
+
+                if (controlador.validarAcceso(pass)) {
+                    System.out.println("Acceso correcto.");
+
+                    boolean salir = false;
+                    while (!salir) {
+                        System.out.println("**********************");
+                        System.out.println("1. Modificar Contraseña");
+                        System.out.println("2. Salir");
+                        System.out.println("**********************");
+                        System.out.print("Elige opción: ");
+                        int op = Integer.parseInt(sc.nextLine());
+
+                        switch (op) {
+                            case 1:
+                                System.out.print("Contraseña actual: ");
+                                String actual = sc.nextLine();
+                                System.out.print("Nueva contraseña: ");
+                                String nueva = sc.nextLine();
+
+                                if (controlador.cambiarContrasena(actual, nueva)) {
+                                    System.out.println("Contraseña modificada correctamente.");
+                                }else {
+                                    System.out.println("Error al modificar la contraseña.");
+                                }
+                                break;
+                            case 2:
+                                salir = true;
+                                break;
+                            default:
+                                System.out.println("Opción no válida.");
+                        }
+                    }
+
+                } else {
+                    System.out.println("Acceso denegado.");
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
